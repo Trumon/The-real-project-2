2838db8436901a9276be8123e09ea23c72759096
Trumon A Latimer <144071590+Trumon@users.noreply.github.com>
2023-11-08 10:09:38 -0500

    Trumon's project 2 first commit

--- sketch.js
+++ sketch.js
@@ -1,64 +1,65 @@
-// a simple game where you click to drop a ball and 
-// try to get as many balls as possible on the spinning shape
-// move the mouse up and down to change the rotation speed of the shape
-// use the right and left keys to make the shape larger or smaller
+let ball, floor;
+let base;
+let x=0; 
+let basee; 
+let player, wall;
 
-let spinningShape;
-let highScore = 0;
+function setup(){
+    new Canvas(500,100);
+    wall= new Sprite(250,50,30,400, 's');
+    wall.color= 'purple';
+    player= new Sprite(50,50); 
+}
 
 function setup() {
-    let canvas = new Canvas("fullscreen");
+    let canvas= new Canvas(5000,5000,"fullsrceen");
+    world.gravity.y=30;
+    noLoop();
+    
 
-    world.gravity.y = 10;
+    ball = new Sprite();
+    ball.diameter = 300; 
+    ball.y =1;
 
-    // this sprite can be created on a single line, but it's easier to read this way:
-	spinningShape = new Sprite();
-	spinningShape.width = canvas.width/5;
-	spinningShape.height = spinningShape.width;
-    spinningShape.collider = "kinematic";
+    base = new Sprite();
+    base.width = 100;
+    base.height = 100;
+    base.y= 5000 ;
 
-    textFont("Courier", 24);
+    base = new Sprite();
+    base.width = 1000;
+    base.height = 100;
+    base.y= 1;
+
+    floor= new Sprite();
+    floor.y=5000;
+    floor.w=5000;
+    floor.h=500;
+    floor.collider= 'static';
+    floor.drag = 0;
+    floor.bounciness=0;
+
+    basee = new Sprite(); 
+    basee.width = 500;
+    basee.height = 500;
+    basee.y= 1;
+    basee.rotation=45;
+   
 }
 
 function draw() {
-
-    // try the game without this line :)
     clear();
-
-    // the map function translates a value from one range to another
-    // https://p5js.org/reference/#/p5/map
-    spinningShape.rotationSpeed = map(mouse.y, 0, canvas.height, -10, 10);
-
-    // create a ball when the mouse is clicked
-    // https://p5play.org/learn/input_devices.html
-    if (mouse.presses()) {
-        let ball = new Sprite(mouse.x, -20, 20);
-        // make the ball resist rolling when it touches the spinning shape
-        // https://p5play.org/learn/sprite.html?page=9
-        ball.rotationDrag = 10;
+    x=x+0.1;
+    if(x>500){
+        x=0;
     }
+     
+}
 
-    // make the spinning shape larger or smaller using the keyboard
-    // note the difference between presses (above) and pressing (here)
-    if (kb.pressing('right')) {
-        spinningShape.width += 10;
-    } else if (kb.pressing('left')) {
-        spinningShape.width -= 10;
-    }
+function mousePressed(){
+    noLoop();
+}
 
-    // loop through the allSprites array and see how many are above the center of the screen
-    let currentScore = 0;
-    for (let sprite of allSprites) {
-        if (sprite.y < canvas.height/2) {
-            currentScore++;
-            if (currentScore > highScore) {
-                highScore = currentScore;
-            }
-        }
-    }
-
-    // display the score (minus 1 so it doesn't count the spinning shape)
-    text("BALLS: " + (currentScore-1), 40, 60);
-    text("HIGH:  " + (highScore-1), 40, 86);
-
+function mouseReleased(){
+    loop();
 }
